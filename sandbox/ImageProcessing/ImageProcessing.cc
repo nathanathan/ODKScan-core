2a0150c979ee5729fa30e56acc5b78ac3aa0a72d
Baron Von Oldenburg <baronv@cs.uw.edu>
2011-06-05 19:40:30 -0700

    separated file crawling code into new header/cc files
     On branch master
     Your branch is ahead of 'origin/master' by 2 commits.
    
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	new file:   FileUtils.cc
    	new file:   FileUtils.h
    	modified:   ImageProcessing.cc
    	modified:   suite

--- sandbox/ImageProcessing/ImageProcessing.cc
+++ sandbox/ImageProcessing/ImageProcessing.cc
@@ -8,19 +8,7 @@
 #include <vector>
 
 #include "./ImageProcessing.h"
-
-extern "C" {
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <assert.h>
-#include <string.h>
-#include <errno.h>
-}
+#include "./FileUtils.h"
 
 using namespace std;
 using namespace cv;
@@ -41,12 +29,6 @@ void configCornerArray(vector<Point2f>& corners, Point2f* corners_a);
 void straightenImage(const Mat& input_image, Mat& output_image);
 bubble_val checkBubble(Mat& det_img_gray, Point2f& bubble_location);
 
-// recursive helper method to crawl directories
-static void HandleDir(char *dirpath, DIR *d, vector<string> &filenames);
-
-// crawls a directory rootdir for filenames and appends them to the filenames
-// vector parameter
-int CrawlFileTree(char *rootdir, vector<string> &filenames);
 
 vector<bubble_val> ProcessImage(string &imagefilename, string &bubblefilename, float &weight) {
   cout << "debug level is: " << DEBUG << endl;
@@ -292,120 +274,4 @@ void train_PCA_classifier() {
   #if DEBUG > 0
   cout << "finished training PCA classifier" << endl;
   #endif
-}
-
-int CrawlFileTree(char *rootdir, vector<string> &filenames) {
-  struct stat rootstat;
-  int result;
-  DIR *rd;
-
-  // Verify that rootdir is a directory.
-  result = lstat((char *) rootdir, &rootstat);
-  if (result == -1) {
-    // We got some kind of error stat'ing the file. Give up
-    // and return an error.
-    return 0;
-  }
-  if (!S_ISDIR(rootstat.st_mode)) {
-    // It isn't a directory, so give up.
-    return 0;
-  }
-
-  // Try to open the directory using opendir().  If try but fail,
-  // (e.g., we don't have permissions on the directory), return NULL.
-  // ("man 3 opendir")
-  rd = opendir(rootdir);
-  if (rd == NULL) {
-    return 0;
-  }
-
-  // Begin the recursive handling of the directory.
-  HandleDir(rootdir, rd, filenames);
-
-  // All done, free up.
-  assert(closedir(rd) == 0);
-  return 1;
-}
-
-
-static void HandleDir(char *dirpath, DIR *d, vector<string> &filenames) {
-  // Loop through the directory.
-  while (1) {
-    char *newfile;
-    int res, charsize;
-    struct stat nextstat;
-    struct dirent *dirent = NULL;
-
-    // Use the "readdir()" system call to read the next directory
-    // entry. (man 3 readdir).  If we hit the end of the
-    // directory (i.e., readdir returns NULL and errno == 0),
-    // return back out of this function.  Note that you have
-    // access to the errno global variable as a side-effect of
-    // us having #include'd <errno.h>.
-    errno = 0;
-
-    dirent = readdir(d);
-
-    if (dirent == NULL && errno == 0) return;
-
-    // If the directory entry is named "." or "..",
-    // ignore it.  (use the C "continue;" expression
-    // to begin the next iteration of the while() loop.)
-    // You can find out the name of the directory entry
-    // through the "d_name" field of the struct dirent
-    // returned by readdir(), and you can use strcmp()
-    // to compare it to "." or ".."
-    if ((strcmp(dirent->d_name, ".") == 0) ||
-        (strcmp(dirent->d_name, "..") == 0))
-      continue;
-
-    // We need to append the name of the file to the name
-    // of the directory we're in to get the full filename.
-    // So, we'll malloc space for:
-    //
-    //     dirpath + "/" + dirent->d_name + '\0'
-    charsize = strlen(dirpath) + 1 + strlen(dirent->d_name) + 1;
-    newfile = (char *) malloc(charsize);
-    assert(newfile != NULL);
-    if (dirpath[strlen(dirpath)-1] == '/') {
-      // no need to add an additional '/'
-      snprintf(newfile, charsize, "%s%s", dirpath, dirent->d_name);
-    } else {
-      // we do need to add an additional '/'
-      snprintf(newfile, charsize, "%s/%s", dirpath, dirent->d_name);
-    }
-
-    // Use the "lstat()" system call to ask the operating system
-    // to give us information about the file named by the
-    // directory entry.   ("man lstat")
-    res = lstat(newfile, &nextstat);
-    if (res == 0) {
-      // Test to see if the file is a "regular file" using
-      // the S_ISREG() macro described in the lstat man page.
-      // If so, process the file by invoking the HandleFile()
-      // private helper function.
-      //
-      // On the other hand, if the file turns out to be a
-      // directory (which you can find out using the S_ISDIR()
-      // macro described on the same page, then you need to
-      // open the directory using opendir()  (man 3 opendir)
-      // and recursively invoke HandleDir to handle it.
-      // Be sure to call the "closedir()" system call
-      // when the recursive HandleDir() returns to close the
-      // opened directory.
-
-      if (S_ISREG(nextstat.st_mode))
-        filenames.push_back(newfile);
-
-      if (S_ISDIR(nextstat.st_mode)) {
-        DIR* d = opendir(newfile);
-        HandleDir(newfile, d, filenames);
-        closedir(d);
-      }
-    }
-
-    // Done with this file.  Fall back up to the next
-    // iteration of the while() loop.
-    free(newfile);
-  }
 }
