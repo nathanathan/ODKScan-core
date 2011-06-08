038e01b2e2c50c0a94c6e24f87304305e9e61c51
Baron Von Oldenburg <baronv@cs.uw.edu>
2011-06-07 18:27:28 -0700

    updated ImageProcessing.cc to reflect straightening improvements
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   ImageProcessing.cc
    	modified:   bubble-offsets.txt
    	modified:   suite

--- sandbox/ImageProcessing/ImageProcessing.cc
+++ sandbox/ImageProcessing/ImageProcessing.cc
@@ -30,7 +30,9 @@ using namespace cv;
 #define SEGMENT_HEIGHT 200
 
 // buffer around segment in pixels
-#define SEGMENT_BUFFER 70 
+#define SEGMENT_BUFFER 70
+
+#define EIGENBUBBLES 5
 
 #define DEBUG 1
 
@@ -40,15 +42,47 @@ vector <bubble_val> training_bubble_values;
 vector <Point2f> training_bubbles_locations;
 float weight_param;
 string imgfilename;
+Point search_window(10, 10);
 
 void configCornerArray(vector<Point2f>& corners, Point2f* corners_a);
 void straightenImage(const Mat& input_image, Mat& output_image);
-double rateBubble(Mat& det_img_gray, Point bubble_location, PCA& my_PCA);
-bubble_val checkBubble(Mat& det_img_gray, Point bubble_location, Point search_window=Point(5,5));
+double rateBubble(Mat& det_img_gray, Point bubble_location);
+bubble_val checkBubble(Mat& det_img_gray, Point bubble_location);
 void getSegmentLocations(vector<Point2f> &segmentcorners, string segfile);
-vector<bubble_val> processSegment(Mat &segment, string bubble_offsets, int *top, int *left);
+vector<bubble_val> processSegment(Mat &segment, string bubble_offsets);
 Mat getSegmentMat(Mat &img, Point2f &corner);
 void find_bounding_lines(Mat& img, int* upper, int* lower, bool vertical);
+void align_segment(Mat& img, Mat& aligned_segment);
+
+template <class Tp>
+void configCornerArray(vector<Tp>& orig_corners, Point2f* corners_a, float expand) {
+  float min_dist;
+  int min_idx;
+  float dist;
+  
+  vector<Point2f> corners;
+  
+  for(int i = 0; i < orig_corners.size(); i++ ){
+  	corners.push_back(Point2f(float(orig_corners[i].x), float(orig_corners[i].y)));
+  }
+  //Make sure the form corners map to the correct image corner
+  //by snaping the nearest form corner to each image corner.
+  for(int i = 0; i < 4; i++) {
+    min_dist = FLT_MAX;
+    for(int j = 0; j < corners.size(); j++ ){
+      dist = norm(corners[j]-corners_a[i]);
+      if(dist < min_dist){
+        min_dist = dist;
+        min_idx = j;
+      }
+    }
+    corners_a[i]=corners[min_idx] + expand * (corners_a[i] - corners[min_idx]);
+    //Two relatively minor reasons for doing this,
+    //1. Small efficiency gain
+    //2. If 2 corners share a closest point this resolves the conflict.
+    corners.erase(corners.begin()+min_idx);
+  }
+}
 
 vector<vector<bubble_val> > ProcessImage(string &imagefilename, string &bubblefilename, float &weight) {
   #if DEBUG > 0
@@ -104,34 +138,12 @@ vector<vector<bubble_val> > ProcessImage(string &imagefilename, string &bubblefi
   cout << "processing all segments" << endl;
   #endif
   for (vector<Mat>::iterator it = segmats.begin(); it != segmats.end(); it++) {
-    int top = 0, bottom = 0, left = 0, right = 0;
-
-    #if DEBUG > 1
-    cout << "finding top and bottom bounding lines" << endl;
-    #endif
-    find_bounding_lines(*it, &top, &bottom, true);
-    #if DEBUG > 1
-    cout << "finding left and right bounding lines" << endl;
-    #endif
-    find_bounding_lines(*it, &left, &right, false);
-
-    #if DEBUG > 1
-    cout << "processing segment" << endl;
-    #endif
-    segment_results.push_back(processSegment(*it, buboffsetfile, &top, &left));
-
-    #if DEBUG > 1
-    cout << "top is: " << top << endl;
-    cout << "bottom is: " << bottom << endl;
-    cout << "left is: " << left << endl;
-    cout << "right is: " << right << endl;
-    #endif
-    #if DEBUG > 0
-    (*it).col(top)+=200;
-    (*it).col(bottom)+=200;
-    (*it).row(left)+=200;
-    (*it).row(right)+=200;
-    #endif
+    //wouldn't be a bad idea memory wise to merge this loop with the above loop
+    Mat aligned_segment((*it).rows - SEGMENT_BUFFER, (*it).cols - SEGMENT_BUFFER, CV_8UC1);
+	
+    align_segment(*it, aligned_segment);
+    segment_results.push_back(processSegment(aligned_segment, buboffsetfile));
+    aligned_segment.copyTo(*it);
   }
 
   #if DEBUG > 0
@@ -143,7 +155,6 @@ vector<vector<bubble_val> > ProcessImage(string &imagefilename, string &bubblefi
     string segfilename("marked_");
     segfilename.push_back((char)i+33);
     segfilename.append(".jpg");
-    //cout << segfilename << endl;
     imwrite(segfilename, segmats[i]);
   }
   #endif
@@ -151,7 +162,84 @@ vector<vector<bubble_val> > ProcessImage(string &imagefilename, string &bubblefi
   return segment_results;
 }
 
-vector<bubble_val> processSegment(Mat &segment, string bubble_offsets, int *top, int *left) {
+void align_segment(Mat& img, Mat& aligned_segment){
+  vector < vector<Point> > contours;
+  vector < vector<Point> > borderContours;
+  vector < Point > approx;
+  vector < Point > maxRect;
+	
+  //Threshold the image
+  //Maybe we should dilate or blur or something first?
+  //The ideal image would be black lines and white boxes with nothing in them
+  //so if we can filter to get something closer to that, it is a good thing.
+  Scalar my_mean;
+  Scalar my_stddev;
+  meanStdDev(img, my_mean, my_stddev);
+  Mat imgThresh = img > (my_mean.val[0]-.05*my_stddev.val[0]);
+	
+  // Find all external contours of the image
+  findContours(imgThresh, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+	
+  float maxContourArea = 0;
+  // Iterate through all detected contours
+  for (size_t i = 0; i < contours.size(); ++i) {
+    // reduce the number of points in the contour
+    approxPolyDP(Mat(contours[i]), approx,
+                     arcLength(Mat(contours[i]), true) * 0.1, true);
+
+    float area = fabs(contourArea(Mat(approx)));
+
+    if (area > maxContourArea) {
+      maxRect = approx;
+			maxContourArea = area;
+		}
+		//Maybe I could refine this by using a corner detector and using
+		//the 4 contour points with the highest responce?
+	}
+	if ( maxRect.size() == 4 && isContourConvex(Mat(maxRect)) && maxContourArea > (img.cols/2) * (img.rows/2)) {
+		Point2f segment_corners[4] = {Point2f(0,0),Point2f(aligned_segment.cols,0),
+			Point2f(0,aligned_segment.rows),Point2f(aligned_segment.cols,aligned_segment.rows)};
+		Point2f corners_a[4] = {Point2f(0,0),Point2f(img.cols,0),
+			Point2f(0,img.rows),Point2f(img.cols,img.rows)};
+		
+		configCornerArray(maxRect, corners_a, .1);
+		Mat H = getPerspectiveTransform(corners_a , segment_corners);
+		warpPerspective(img, aligned_segment, H, aligned_segment.size());
+	}
+	else{//use the bounding line method if the contour method fails
+		int top = 0, bottom = 0, left = 0, right = 0;
+    	find_bounding_lines(img, &top, &bottom, false);
+    	find_bounding_lines(img, &left, &right, true);
+    	
+		//debug stuff
+		/*
+		img.copyTo(aligned_segment);
+		const Point* p = &maxRect[0];
+		int n = (int) maxRect.size();
+		polylines(aligned_segment, &p, &n, 1, true, 200, 2, CV_AA);
+		
+		img.row(top)+=200;
+    	img.row(bottom)+=200;
+    	img.col(left)+=200;
+    	img.col(right)+=200;
+		img.copyTo(aligned_segment);
+		*/
+
+    	float bounding_lines_threshold = .2;
+    	if ((abs((bottom - top) - aligned_segment.rows) < bounding_lines_threshold * aligned_segment.rows) &&
+    		(abs((right - left) - aligned_segment.cols) < bounding_lines_threshold * aligned_segment.cols) &&
+    		top + aligned_segment.rows < img.rows &&  top + aligned_segment.cols < img.cols) {
+    		
+    		img(Rect(left, top, aligned_segment.cols, aligned_segment.rows)).copyTo(aligned_segment);
+    		
+    	}
+    	else{
+    		img(Rect(SEGMENT_BUFFER, SEGMENT_BUFFER, aligned_segment.cols, aligned_segment.rows)).copyTo(aligned_segment);
+    	}
+	}
+}
+
+vector<bubble_val> processSegment(Mat &segment, string bubble_offsets) {
   vector<Point2f> bubble_locations;
   vector<bubble_val> retvals;
   string line;
@@ -165,7 +253,7 @@ vector<bubble_val> processSegment(Mat &segment, string bubble_offsets, int *top,
 
         ss >> bubx;
         ss >> buby;
-        Point2f bubble(bubx + *top, buby + *left);
+        Point2f bubble(bubx, buby + 3);
         bubble_locations.push_back(bubble);
       }
     }
@@ -343,31 +431,8 @@ void configCornerArray(vector<Point2f>& corners, Point2f* corners_a){
   #endif
 }
 
-//Assuming there is a bubble at the specified location,
-//this function will tell you if it is filled, empty, or probably not a bubble.
-/*
-bubble_val checkBubble(Mat& det_img_gray, Point2f& bubble_location) {
-  #if DEBUG > 1
-  cout << "in checkBubble" << endl;
-  cout << "checking bubble location: " << bubble_location << endl;
-  #endif
-  Mat query_pixels;
-  getRectSubPix(det_img_gray, Point(14,18), bubble_location, query_pixels);
-  query_pixels.convertTo(query_pixels, CV_32F);
-  //normalize(query_pixels, query_pixels);
-  transpose(my_PCA.project(query_pixels.reshape(0,1)), query_pixels);
-  Mat M = (Mat_<float>(3,1) << weight_param, 1.0 - weight_param, 1.0);
-  query_pixels = comparison_vectors*query_pixels.mul(M);
-  Point max_location;
-  minMaxLoc(query_pixels, NULL, NULL, NULL, &max_location);
-  #if DEBUG > 1
-  cout << "exiting checkBubble" << endl;
-  #endif
-  return training_bubble_values[max_location.y];
-}*/
-
 //Rate a location on how likely it is to be a bubble
-double rateBubble(Mat& det_img_gray, Point bubble_location, PCA& my_PCA){
+double rateBubble(Mat& det_img_gray, Point bubble_location) {
     Mat query_pixels, pca_components;
     getRectSubPix(det_img_gray, Point(EXAMPLE_WIDTH,EXAMPLE_HEIGHT), bubble_location, query_pixels);
     query_pixels.reshape(0,1).convertTo(query_pixels, CV_32F);
@@ -378,7 +443,7 @@ double rateBubble(Mat& det_img_gray, Point bubble_location, PCA& my_PCA){
 }
 
 //Compare the bubbles with all the bubbles used in the classifier.
-bubble_val checkBubble(Mat& det_img_gray, Point bubble_location, Point search_window){
+bubble_val checkBubble(Mat& det_img_gray, Point bubble_location) {
     Mat query_pixels;
     //This bit of code finds the location in the search_window most likely to be a bubble
     //then it checks that rather than the exact specified location.
@@ -386,7 +451,7 @@ bubble_val checkBubble(Mat& det_img_gray, Point bubble_location, Point search_wi
     Point offset = Point(bubble_location.x - search_window.x/2, bubble_location.y - search_window.y/2);
     for(size_t i = 0; i < search_window.y; i+=1) {
         for(size_t j = 0; j < search_window.x; j+=1) {
-            out.row(i).col(j) += rateBubble(det_img_gray, Point(j,i) + offset, my_PCA);
+            out.row(i).col(j) += rateBubble(det_img_gray, Point(j,i) + offset);
         }
     }
     Point min_location;
@@ -426,7 +491,6 @@ void train_PCA_classifier() {
 
  // Set training_bubble_values here
   training_bubble_values.push_back(FILLED_BUBBLE);
-  training_bubble_values.push_back(FILLED_BUBBLE);
   training_bubble_values.push_back(EMPTY_BUBBLE);
   training_bubble_values.push_back(FILLED_BUBBLE);
   training_bubble_values.push_back(FILLED_BUBBLE);
@@ -434,6 +498,7 @@ void train_PCA_classifier() {
   training_bubble_values.push_back(FILLED_BUBBLE);
   training_bubble_values.push_back(FILLED_BUBBLE);
   training_bubble_values.push_back(EMPTY_BUBBLE);
+  training_bubble_values.push_back(FILLED_BUBBLE);
   training_bubble_values.push_back(FILLED_BUBBLE);
 
   Mat example_strip = imread("example_strip.jpg");
@@ -450,6 +515,6 @@ void train_PCA_classifier() {
     PCA_set.row(i) += PCA_set_row.reshape(0,1);
   }
 
-  my_PCA = PCA(PCA_set, Mat(), CV_PCA_DATA_AS_ROW, 3);
+  my_PCA = PCA(PCA_set, Mat(), CV_PCA_DATA_AS_ROW, EIGENBUBBLES);
   comparison_vectors = my_PCA.project(PCA_set);
 }
