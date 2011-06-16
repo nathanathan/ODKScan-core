cfc3d70300de97e43122f71682299cc516862db7
Nathan Breit <nbnate@gmail.com>
2011-06-16 15:21:27 -0700

    Fixed a bug with image labels and improved the alignment so that we don't have any misaligned segments now on the test form. Now it's time to work on improving the classifier.

--- sandbox/ImageProcessing/ImageProcessing.cc
+++ sandbox/ImageProcessing/ImageProcessing.cc
@@ -10,12 +10,15 @@
 
 #include "./FileUtils.h"
 
-/*
- * processing constants
- */
+//Image straightening constants
 #define DILATION 6
 #define BLOCK_SIZE 3
 #define DIST_PARAM 500
+
+//image_align constants
+#define THRESH_OFFSET_LB -.3
+#define THRESH_DECR_SIZE .05
+
 
 // how wide is the segment in pixels
 #define SEGMENT_WIDTH 144
@@ -29,7 +32,7 @@
 #define SEGMENT_BUFFER 70
 
 #define DEBUG 0
-#define DEBUG_ALIGN_SEGMENT 0
+#define DEBUG_ALIGN_IMAGE 0
 
 using namespace cv;
 
@@ -46,7 +49,6 @@ string get_unique_name(string prefix);
 vector<bubble_val> processSegment(Mat &segment, string bubble_offsets);
 Mat getSegmentMat(Mat &img, Point2f &corner);
 void find_bounding_lines(Mat& img, int* upper, int* lower, bool vertical);
-void align_segment(Mat& img, Mat& aligned_segment);
 
 
 //Takes a vector of found corners, and an array of destination corners they should map to
@@ -77,7 +79,6 @@ void configCornerArray(vector<Tp>& found_corners, Point2f* dest_corners, float e
 		corners.erase(corners.begin()+min_idx);
 	}
 }
-
 void find_bounding_lines(Mat& img, int* upper, int* lower, bool vertical) {
   int center_size = 20 * SCALEPARAM;
   Mat grad_img, out;
@@ -96,35 +97,33 @@ void find_bounding_lines(Mat& img, int* upper, int* lower, bool vertical) {
   *upper = min_location_top.y;
   *lower = min_location_bottom.y + out.rows/2 + center_size;
 }
-//Threshold the image
-//The ideal image would be black lines and white boxes with nothing in them
-//so if we can filter to get something closer to that, it is a good thing.
-void my_threshold(Mat& img, Mat& thresholded_img) {
+//Thresholds the image using its mean pixel intensity.
+//thresh_offset specified an offset in units of standard deviations.
+void my_threshold(Mat& img, Mat& thresholded_img, float thresh_offset) {
+	//The ideal image would be black lines and white boxes with nothing in them
+	//so if we can filter to get something closer to that, it is a good thing.
 	//One of the big problems with thresholding is that it is thrown off by filled in bubbles.
 	//Equalizing seems to mitigate this somewhat.
-	//It might help use the same threshold for all the segments, but if one is in shadow
+	//It might help use the same threshold level for all the segments, but if one is in shadow
 	//it will cause problems.
 	Mat equalized_img;
 	equalizeHist(img, equalized_img);
 	Scalar my_mean;
 	Scalar my_stddev;
 	meanStdDev(equalized_img, my_mean, my_stddev);
-	thresholded_img = equalized_img > (my_mean.val[0] - .5* my_stddev.val[0]);
+	//Everything before this point could be precompted in some cases.
+	//If we ever need to speed things up that is something to consider,
+	//however debuggging is easier this way. 
+	thresholded_img = equalized_img > (my_mean.val[0] - thresh_offset * my_stddev.val[0]);
 }
-void align_segment(Mat& img, Mat& aligned_segment){
+float findRectContour(Mat& img, vector<Point>& maxRect){
 	vector < vector<Point> > contours;
 	vector < Point > approx;
-	vector < Point > maxRect;
-	
-	Mat dbg_out, imgThresh;
-	my_threshold(img, imgThresh);
-	
-	#if DEBUG_ALIGN_SEGMENT > 0
-	imgThresh.convertTo(dbg_out, CV_8U);
-	#endif
-	
+	// Need this to prevent find contours from destroying the image.
+	Mat img2;
+	img.copyTo(img2);
 	// Find all external contours of the image
-	findContours(imgThresh, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+	findContours(img2, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
 	float maxContourArea = 0;
 	// Iterate through all detected contours
@@ -140,114 +139,145 @@ void align_segment(Mat& img, Mat& aligned_segment){
 			maxContourArea = area;
 		}
 	}
-	#if DEBUG_ALIGN_SEGMENT > 0
+	return maxContourArea;
+}
+// This function take an image looks for a large rectangle.
+// If it finds one it transforms that rectangle so that it fits into aligned_image.
+// This can be used in place of straighten image, however it does not seem to
+// produce as nice of results.
+void align_image(Mat& img, Mat& aligned_image, float thresh_seed = 1.0){
+	Mat imgThresh;
+	vector<Point> maxRect;
+	float maxContourArea;
+	
+	// Find an appropriately size rectangular section of an image
+	// by iteratively decreasing the threshold offset (which means more black).
+	while( true ){
+		my_threshold(img, imgThresh, thresh_seed);
+		maxContourArea = findRectContour(imgThresh, maxRect);
+		thresh_seed -= THRESH_DECR_SIZE;
+		if(thresh_seed < THRESH_OFFSET_LB)
+			break;
+		if( maxRect.size() == 4 && isContourConvex(Mat(maxRect)) && maxContourArea > (img.cols/2) * (img.rows/2))
+			break;
+	}
+	
+	#if DEBUG_ALIGN_IMAGE > 0
+	Mat dbg_out;
+	imgThresh.convertTo(dbg_out, CV_8U);
 	string directory = "debug_segment_images";
 	directory.append("/");
 	string segfilename = get_unique_name("alignment_debug_");
 	segfilename.append(".jpg");
 	imwrite(directory+segfilename, dbg_out);
 	#endif
+	
 	if ( maxRect.size() == 4 && isContourConvex(Mat(maxRect)) && maxContourArea > (img.cols/2) * (img.rows/2)) {
-		Point2f segment_corners[4] = {Point2f(0,0),Point2f(aligned_segment.cols,0),
-		Point2f(0,aligned_segment.rows),Point2f(aligned_segment.cols,aligned_segment.rows)};
+		Point2f segment_corners[4] = {Point2f(0,0),Point2f(aligned_image.cols,0),
+		Point2f(0,aligned_image.rows),Point2f(aligned_image.cols,aligned_image.rows)};
 		Point2f corners_a[4] = {Point2f(0,0),Point2f(img.cols,0),
 		Point2f(0,img.rows),Point2f(img.cols,img.rows)};
 
 		configCornerArray(maxRect, corners_a, .1);
 		Mat H = getPerspectiveTransform(corners_a , segment_corners);
-		warpPerspective(img, aligned_segment, H, aligned_segment.size());
+		warpPerspective(img, aligned_image, H, aligned_image.size());
 	}
 	else{//use the bounding line method if the contour method fails
 		int top = 0, bottom = 0, left = 0, right = 0;
 		find_bounding_lines(img, &top, &bottom, false);
 		find_bounding_lines(img, &left, &right, true);
 
-		#if DEBUG_ALIGN_SEGMENT > 0
-		img.copyTo(aligned_segment);
+		#if DEBUG_ALIGN_IMAGE > 0
+		img.copyTo(dbg_out);
 		const Point* p = &maxRect[0];
 		int n = (int) maxRect.size();
-		polylines(aligned_segment, &p, &n, 1, true, 200, 2, CV_AA);
+		polylines(dbg_out, &p, &n, 1, true, 200, 2, CV_AA);
 
-		aligned_segment.row(top)+=200;
-		aligned_segment.row(bottom)+=200;
-		aligned_segment.col(left)+=200;
-		aligned_segment.col(right)+=200;
-		#else
+		dbg_out.row(top)+=200;
+		dbg_out.row(bottom)+=200;
+		dbg_out.col(left)+=200;
+		dbg_out.col(right)+=200;
+		string directory = "debug_segment_images";
+		directory.append("/");
+		string segfilename = get_unique_name("alignment_debug_");
+		segfilename.append(".jpg");
+		imwrite(directory+segfilename, dbg_out);
+		#endif
+		
 		float bounding_lines_threshold = .2;
-		if ((abs((bottom - top) - aligned_segment.rows) < bounding_lines_threshold * aligned_segment.rows) &&
-			(abs((right - left) - aligned_segment.cols) < bounding_lines_threshold * aligned_segment.cols) &&
-			top + aligned_segment.rows < img.rows &&  top + aligned_segment.cols < img.cols) {
+		if ((abs((bottom - top) - aligned_image.rows) < bounding_lines_threshold * aligned_image.rows) &&
+			(abs((right - left) - aligned_image.cols) < bounding_lines_threshold * aligned_image.cols) &&
+			top + aligned_image.rows < img.rows &&  top + aligned_image.cols < img.cols) {
 
-			img(Rect(left, top, aligned_segment.cols, aligned_segment.rows)).copyTo(aligned_segment);
+			img(Rect(left, top, aligned_image.cols, aligned_image.rows)).copyTo(aligned_image);
 
 		}
 		else{
 			img(Rect(SEGMENT_BUFFER * SCALEPARAM, SEGMENT_BUFFER * SCALEPARAM,
-				aligned_segment.cols, aligned_segment.rows)).copyTo(aligned_segment);
+				aligned_image.cols, aligned_image.rows)).copyTo(aligned_image);
 		}
-		#endif
 	}
 }
-
 vector< vector<bubble_val> > ProcessImage(string &imagefilename, string &bubblefilename, float &weight) {
-  #if DEBUG > 0
-  cout << "debug level is: " << DEBUG << endl;
-  #endif
-  string seglocfile("segment-offsets-tmp.txt");
-  string buboffsetfile("bubble-offsets.txt");
-  set_weight(weight);
-  vector < Point2f > corners, segment_locations;
-  vector<bubble_val> bubble_vals;
-  vector<vector<bubble_val> > segment_results;
-  Mat img, imgGrey, out, warped;
-  imgfilename = imagefilename;
+	#if DEBUG > 0
+	cout << "debug level is: " << DEBUG << endl;
+	#endif
+	string seglocfile("segment-offsets-tmp.txt");
+	string buboffsetfile("bubble-offsets.txt");
+	set_weight(weight);
+	vector < Point2f > corners, segment_locations;
+	vector<bubble_val> bubble_vals;
+	vector<vector<bubble_val> > segment_results;
+	Mat img, imgGrey, out, warped;
+	imgfilename = imagefilename;
 
-  // Read the input image
-  img = imread(imagefilename);
-  if (img.data == NULL) {
-    return vector<vector<bubble_val> >();
-  }
+	// Read the input image
+	img = imread(imagefilename);
+	if (img.data == NULL) {
+	return vector<vector<bubble_val> >();
+	}
 
-  #if DEBUG > 0
-  cout << "converting to grayscale" << endl;
-  #endif
-  cvtColor(img, imgGrey, CV_RGB2GRAY);
+	#if DEBUG > 0
+	cout << "converting to grayscale" << endl;
+	#endif
+	cvtColor(img, imgGrey, CV_RGB2GRAY);
 
-  Mat straightened_image(3300 * SCALEPARAM, 2550 * SCALEPARAM, CV_8U);
+	Mat straightened_image(3300 * SCALEPARAM, 2550 * SCALEPARAM, CV_8U);
 
-  #if DEBUG > 0
-  cout << "straightening image" << endl;
-  #endif
-  straightenImage(imgGrey, straightened_image);
-  
-  #if DEBUG > 0
-  cout << "writing to output image" << endl;
-  imwrite("straightened_" + imagefilename, straightened_image);
-  #endif
-  
-  #if DEBUG > 0
-  cout << "getting segment locations" << endl;
-  #endif
-  getSegmentLocations(segment_locations, seglocfile);
+	#if DEBUG > 0
+	cout << "straightening image" << endl;
+	#endif
+	straightenImage(imgGrey, straightened_image);
+	//align_image(imgGrey, straightened_image, 1.0);
 
-  #if DEBUG > 0
-  cout << "creating and processing segment images" << endl;
-  #endif
-  for (vector<Point2f>::iterator it = segment_locations.begin();
-       it != segment_locations.end(); it++) {
-    Point2f loc((*it).x * SCALEPARAM, (*it).y * SCALEPARAM);
-    
-    Mat segment = getSegmentMat(straightened_image, loc);
-    Mat aligned_segment(segment.rows - (SEGMENT_BUFFER * SCALEPARAM),
-                        segment.cols - (SEGMENT_BUFFER * SCALEPARAM), CV_8UC1);
-    #if DEBUG > 0
-    cout << "aligning segment" << endl;
-    #endif
-    align_segment(segment, aligned_segment);
-    segment_results.push_back(processSegment(aligned_segment, buboffsetfile));
-  }
+	#if DEBUG > 0
+	cout << "writing to output image" << endl;
+	imwrite("straightened_" + imagefilename, straightened_image);
+	#endif
 
-  return segment_results;
+	#if DEBUG > 0
+	cout << "getting segment locations" << endl;
+	#endif
+	getSegmentLocations(segment_locations, seglocfile);
+
+	#if DEBUG > 0
+	cout << "creating and processing segment images" << endl;
+	#endif
+	for (vector<Point2f>::iterator it = segment_locations.begin();
+	   it != segment_locations.end(); it++) {
+		Point2f loc((*it).x * SCALEPARAM, (*it).y * SCALEPARAM);
+
+		Mat segment = getSegmentMat(straightened_image, loc);
+		Mat aligned_segment(segment.rows - (SEGMENT_BUFFER * SCALEPARAM),
+				            segment.cols - (SEGMENT_BUFFER * SCALEPARAM), CV_8UC1);
+		#if DEBUG > 0
+		cout << "aligning segment" << endl;
+		#endif
+		align_image(segment, aligned_segment, .6);
+		segment_results.push_back(processSegment(aligned_segment, buboffsetfile));
+	}
+
+	return segment_results;
 }
 
 string get_unique_name(string prefix) {
@@ -255,12 +285,14 @@ string get_unique_name(string prefix) {
 	int gc_temp = global_counter;
 	while( true ) {
 		ss << (char) ((gc_temp % 10) + '0');
-		gc_temp /= 10;
+		gc_temp = gc_temp / 10;
 		if(gc_temp == 0)
 			break;
 	}
-	global_counter++;
-	prefix.append(ss.str());
+	string temp = ss.str();
+	reverse(temp.begin(), temp.end());
+	prefix.append(temp);
+	global_counter+=1;
 	return prefix;
 }
 
