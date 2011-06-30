b78e262c84c5f07601224d311a26ed6c38014c71
Nathan Breit <nbnate@gmail.com>
2011-06-30 15:45:51 -0700

    Switching to parsing JSON templates.

--- TestSuite/test_ImageProcessing.cpp
+++ TestSuite/test_ImageProcessing.cpp
@@ -1,122 +1,39 @@
+/*
+Description of what's being tested
+*/
+
 #include <iostream>
-#include <fstream>
 #include <string>
+
 #include "ImageProcessing.h"
-
-#include <iterator>
-
-#define DEBUG 0
-
-#define DUMP_BUBBLE_VALS
 
 using namespace std;
 
-void check_values(vector<vector<bubble_val> > &found, int *tpos, int *tneg, int *fpos, int *fneg);
-
-//Maybe this should go in image processing?
-//Although it is likey to change when we switch to JSON templates.
-void writeBubbleVals(string filename, vector< vector<bubble_val> > bubble_vals) {
-	ofstream outfile(filename.c_str(), ios::out | ios::binary);
-	vector< vector<bubble_val> >::iterator it;
-	ostream_iterator<int> oi(outfile, " ");
-	for( it=bubble_vals.begin(); it < bubble_vals.end(); it++) {
-		copy((*it).begin(), (*it).end(), oi);
-		outfile << endl;
-	}
-	outfile.close();
-}
-
+//The reason to use JSON for the bubble-vals files is that other code, like java code can parse them
+//and display the results without any hardcoding.
 int main(int argc, char *argv[]) {
-	 PCA_classifier myClassifier;
-	 myClassifier.train_PCA_classifier();
 
-	// image to be processed
-	string image("vr_simulated.jpg");
-	//string image("booklet_form.jpg");
+	// Image to be processed:
+	// TODO: This (or a variant of it) should eventually use FileUtils and a predicate to specify multiple images.
+	string imagePath("form_images/unbounded_form/A0.jpg");
+	// Template to use:
+	string templatePath("form_templates/unbounded_form_shreddr_cleaned.json");
+	// Location to output results:
+	string outputPath("unbounded_form_A0_vals.json");
 
-	// bubble location file
-	string bubbles("bubble-locations.full2");
+	Processor myProcessor(templatePath);
+	myProcessor.trainClassifier();
+
 	float i;
-
-	std::cout << "parameter_name, parameter_value, true_positives, ";
-	std::cout << "false_positives, true_negatives, false_negatives" << std::endl;
-
 	// testing loop, currently iterates over weight_param
 	#if 1
 	for (i = 0.5; i <= .5; i += 0.05) {
 	#else
 	for (i = 0.0; i <= 1.0; i += 0.05) {
 	#endif
-		myClassifier.set_weight(FILLED_BUBBLE, i);
-		myClassifier.set_weight(PARTIAL_BUBBLE, 1-i);
-		myClassifier.set_weight(EMPTY_BUBBLE, 1-i);
-		
-		// process the image with the given parameter value
-		vector<vector<bubble_val> > bubble_vals = ProcessImage(image, bubbles, myClassifier);
-		
-		#ifdef DUMP_BUBBLE_VALS
-		writeBubbleVals("output-vals-test", bubble_vals);
-		#endif
-		
-		// get true positive, false positive, true negative, and false negative data
-		int tpos = 0, tneg = 0, fpos = 0, fneg = 0;
-		check_values(bubble_vals, &tpos, &tneg, &fpos, &fneg);
-
-		// print out results to console, >> filename to save to file
-		std::cout << "\"weight parameter\", " << i << ", ";
-		std::cout << tpos << ", " << fpos << ", ";
-		std::cout << tneg << ", " << fneg << std::endl;
-	}
-}
-//Compares the found bubble values against those in the bubble-vals file
-void check_values(vector<vector<bubble_val> > &found, int *tpos, int *tneg, int *fpos, int *fneg) {
-	vector<vector<int> > actual;
-	string line;
-	int bubble, i, j;
-
-	// file with actual bubble values, format:
-	//   val val val val val val\n
-	//   val val val val val val\n
-	// etc, example:
-	//   0 0 0 0 0 1 0 0 0 0 0 0 0
-	// reads from left to right within row, top to bottom within segment
-	// one segment per line
-	string valfile("bubble-vals");
-
-	// read in bubble values
-	ifstream bubble_value_file(valfile.c_str());
-	if (bubble_value_file.is_open()) {
-		while (getline(bubble_value_file, line)) {
-			stringstream ss(line);
-
-			vector<int> bubble_line;
-			while (ss.good()) {
-				ss >> bubble;
-				bubble_line.push_back(bubble);
-			}
-
-			actual.push_back(bubble_line);
-		}
-	}
-
-	for (i = 0; i < actual.size(); i++) {
-		for (j = 0; j < actual[i].size(); j++) {
-			// true positive
-			if (found[i][j] != 0 && actual[i][j] == 1) {
-				(*tpos)++;
-			}
-			// true negative
-			else if (found[i][j] == 0 && actual[i][j] == 0) {
-				(*tneg)++;
-			}
-			// false positive
-			else if (found[i][j] != 0 && actual[i][j] == 0) {
-				(*fpos)++;
-			}
-			// false negative
-			else if (found[i][j] == 0 && actual[i][j] == 1) {
-				(*fneg)++;
-			}
-		}
+		myProcessor.setClassifierWeight(i);
+		myProcessor.loadForm(imagePath);
+		myProcessor.alignForm();
+		myProcessor.processForm(outputPath);
 	}
 }
