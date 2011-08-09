30459e3224b37093add25bae42ab638466eb38b8
Nathan Breit <nbnate@gmail.com>
2011-08-09 11:18:09 -0700

    removing files from the jni folder so they can be replaced by the core files folder.

--- jni/FileUtils.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <vector>
-
-// crawls a directory rootdir for filenames and appends them to the filenames
-// vector parameter (modifies filenames vector)
-int CrawlFileTree(std::string rootdir, std::vector<std::string> &filenames);
-int CrawlFileTree(char* rootdir, std::vector<std::string > &filenames);
-bool fileExists(const std::string& filename);
