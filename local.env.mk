03a282660f1ce05e8abd7e20dac32d5bb4421d74
Nathan Breit <nbnate@gmail.com>
2011-08-23 14:00:10 -0700

    Changing NDK and OpenCV targets

--- local.env.mk
+++ local.env.mk
@@ -1,4 +1,4 @@
 #location of android-opencv port of OpenCV to android
-OPENCV_CONFIG=android-opencv.mk
-ANDROID_NDK_ROOT=/home/nathan/android-ndk-r4-crystax
+#OPENCV_CONFIG=android-opencv.mk
+ANDROID_NDK_ROOT=~/android-ndk-r6
 ARM_TARGETS="armeabi armeabi-v7a"
