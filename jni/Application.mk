427d57011ebb9b4dbf6e8201a309843e68fdd420
Nathan Breit <nbnate@gmail.com>
2011-09-12 16:52:27 -0700

    Adding a comment about the imporance of setting the ISA param to your device's ISA.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,3 +1,6 @@
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -fexceptions
 APP_ABI := armeabi-v7a
+#Some devices (ViewPad7) don't support armabi-v7a and you have to use this param instead:
+#to find out if your device is affected run this command: adb shell cat /proc/cpuinf
+#APP_ABI := armeabi
