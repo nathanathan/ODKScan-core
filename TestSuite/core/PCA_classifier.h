e0a3464d73036a259678d86641d13ab263f1a8ad
Nathan Breit <nbnate@gmail.com>
2011-10-11 19:02:11 -0700

    Adding a bit of documentation

--- TestSuite/core/PCA_classifier.h
+++ TestSuite/core/PCA_classifier.h
@@ -13,14 +13,15 @@ This class implements bubble classification using OpenCV's support vector machin
 class PCA_classifier
 {
 	private:	
-		
 		CvSVM statClassifier;
 
 		std::vector<std::string> classifications;
 		int emptyClassificationIndex;
 		
 		cv::PCA my_PCA;
-	
+
+		//The weighting and search window stuff might not be necessairy:
+		//If you aren't me I recommend ignoring it because it doesn't get used at the moment.
 		cv::Size search_window;
 
 		//A matrix for precomputing gaussian weights for the search window
@@ -40,17 +41,24 @@ class PCA_classifier
 		void PCA_set_add(cv::Mat& PCA_set, std::vector<int>& trainingBubbleValues,
 		                 const std::string& filename, bool flipExamples);
 	public:
-		cv::Size exampleSize; //Can I make this immutable to clients (without an accesor function)?
+		cv::Size exampleSize;
 
 		void set_search_window(cv::Size sw);
-		double rateBubble(const cv::Mat& det_img_gray, const cv::Point& bubble_location) const;
-		bool train_PCA_classifier(	const std::vector<std::string>& examplePaths,
-									cv::Size myExampleSize,
-									int eigenvalues = 7,
-									bool flipExamples = false);
-		cv::Point bubble_align(const cv::Mat& det_img_gray, const cv::Point& bubble_location) const;
-		int classifyBubble(const cv::Mat& det_img_gray, const cv::Point& bubble_location) const;
 
+		//Given a image and location in that image rates how similar it is to the training examples
+		//Lower score = more similar
+		double rateBubble(const cv::Mat& det_img_gray, const cv::Point& bubble_location) const;
+		//Returns a refined location for the object being classified
+		//(currently by doing a hill climbing search with rateBubble as the objective function)
+		cv::Point bubble_align(const cv::Mat& det_img_gray, const cv::Point& bubble_location) const;
+
+		bool train_PCA_classifier( const std::vector<std::string>& examplePaths,
+		                           cv::Size myExampleSize,
+		                           int eigenvalues = 7,
+		                           bool flipExamples = false);
+
+		int classifyBubble(const cv::Mat& det_img_gray, const cv::Point& bubble_location) const;
+		
 		bool save(const std::string& outputPath) const;
 		bool load(const std::string& inputPath, const cv::Size& requiredExampleSize);
 };
