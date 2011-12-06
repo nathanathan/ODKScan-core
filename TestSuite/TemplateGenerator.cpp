612fa7764d88b9a30baddb6cb4d3bc0fa8f37bfd
Nathan Breit <nbnate@gmail.com>
2011-12-05 16:54:45 -0800

    Adding TemplateGenerator to make checkbox forms, and a test program very similar to Experiment.cpp

---
+++ TestSuite/TemplateGenerator.cpp
@@ -0,0 +1,111 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "Addons.h"
+#include "AlignmentUtils.h"
+
+#include <json/json.h>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+#define EDIT_VIEW
+
+void inheritJson(Json::Value & child, Json::Value parent) {
+	Json::Value::Members members = child.getMemberNames();
+	for( Json::Value::Members::iterator itr = members.begin() ; itr != members.end() ; itr++ ) {
+		cout << *itr << flush;
+		parent[*itr] = child[*itr];
+	}
+	child = parent;
+}
+void generateSegment(Json::Value segment, Mat& markupImage) {
+
+	//TODO: What happens if points are doubles?
+	Point tl(segment["x"].asInt(), segment["y"].asInt());
+
+	rectangle(markupImage, tl, tl + Point(segment["width"].asInt(), segment["height"].asInt()),
+	          Scalar::all(0), 2);
+
+	const Json::Value bubble_locations = segment["bubble_locations"];
+	for ( size_t k = 0; k < bubble_locations.size(); k++ ) {
+		Point bubbleLocation(jsonToPoint(bubble_locations[k]));
+		//Not quite sure what the best way to deal with padding will be.
+		Point padding(4,4);
+		Point classifer_size = Point(jsonToPoint(segment["classifier_size"])) - padding;
+
+		cout << classifer_size << endl;
+		#ifdef EDIT_VIEW
+			circle(markupImage, tl + bubbleLocation, 3, Scalar(0, 255, 20), -1);
+		#endif
+		rectangle(markupImage, tl + bubbleLocation - .5 * classifer_size,
+		                       tl + bubbleLocation + .5 * classifer_size, Scalar::all(0), 1);
+	}
+}
+void generateField(Json::Value field, Mat& markupImage) {
+	const Json::Value segments = field["segments"];
+		
+	for ( size_t j = 0; j < segments.size(); j++ ) {
+		Json::Value segment = segments[j];
+		inheritJson(segment, field);
+		generateSegment(segment, markupImage);
+	}
+}
+
+bool generateForm(const char* templatePath, Mat& markupImage) {
+
+
+	Json::Value templateRoot;
+	
+	if( !parseJsonFromFile(templatePath, templateRoot) ) return false;
+
+	const Json::Value fields = templateRoot["fields"];
+	
+	for ( size_t i = 0; i < fields.size(); i++ ) {
+		Json::Value field = fields[i];
+		inheritJson(field, templateRoot);
+		generateField(field, markupImage);
+		
+	}
+	return true;
+}
+
+int main(int argc, char *argv[]) {
+
+	Mat markupImage;
+
+	string formPath("form_images/form_templates/base_fiducial_form.jpg");
+	string outPath("form_images/form_templates/checkbox_form.jpg");
+	string templatePath("form_images/form_templates/checkbox_form.json");
+
+	const string winName = "editing window";
+	namedWindow(winName, CV_WINDOW_NORMAL);
+	
+
+
+	for(;;) {
+        	char c = (char)waitKey(0);
+		if( c == '\x1b' ) // esc
+		{
+		    cout << "Exiting ..." << endl;
+		    return 0;
+		}
+		if( c == 'l' )
+		{
+			markupImage = imread(formPath);
+			if(markupImage.empty()) return false;
+
+			if(!generateForm(templatePath.c_str(), markupImage)) return false;
+			imshow( winName, markupImage );
+
+		}
+		if( c == 's' )
+		{
+			imwrite(outPath, markupImage);
+		}
+	}
+}
+
---
+++ TestSuite/tests/Checkbox.cpp
@@ -0,0 +1,140 @@
+/*
+This program runs the image processing pipeline on every image in the specified folder (recusively)
+then prints out stats breaking down the results by image label and pipeline stage.
+*/
+#include "Processor.h"
+#include "FileUtils.h"
+#include "StatCollector.h"
+#include "MarkupForm.h"
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <time.h>
+
+#define OUT_CSV_PATH "results.csv"
+#include <fstream>
+
+using namespace std;
+
+string getLabel(const string& filepath){
+	int start = filepath.find_last_of("/") + 1;
+	return	filepath.substr(start, filepath.find_last_of("_") - start);
+}
+
+int main(int argc, char *argv[]) {
+
+	clock_t init, final;	
+	
+	map<string, StatCollector> collectors;
+
+	string imageDir(argv[1]);
+	//Ensure the experiment dir ends with a '/'
+	if(imageDir[imageDir.size() - 1] != '/') imageDir.append("/");
+	
+	string inputDir("form_images/" + imageDir);
+	string outputDir("aligned_forms/" + imageDir);
+	string expectedJsonFile( "form_images/dne.json" );
+
+	string templatePath("form_images/form_templates/checkbox_form");
+
+	vector<string> filenames;
+	CrawlFileTree(inputDir, filenames);
+
+	vector<string>::iterator it;
+	for(it = filenames.begin(); it != filenames.end(); it++) {
+
+		if( !isImage((*it)) ) continue;
+		
+		string label = getLabel(*it);
+		
+		//Limits the number of images to five for any given condition
+		if(collectors[label].numImages >= 5) continue;
+
+		collectors[label].incrImages();
+		string relativePathMinusExt((*it).substr((*it).find_first_of(inputDir) + inputDir.length(),
+		                            (*it).length()-inputDir.length()-4));
+		string imgOutputPath(outputDir + relativePathMinusExt + ".jpg");
+		string markedupFormOutfile(outputDir + relativePathMinusExt + "_marked.jpg");
+		string jsonOutfile(outputDir + relativePathMinusExt + ".json");
+		
+		cout << "Processing image: " << (*it) << endl;
+		
+		init = clock();
+		
+		Processor myProcessor("../assets/");
+		#define DO_CAMERA_CALIBRATION true
+		if( !myProcessor.loadFormImage((*it).c_str(), DO_CAMERA_CALIBRATION)) {
+			cout << "\E[31m" <<  "Could not load. Arg: " << "\e[0m" << (*it) << endl;
+			//Note that load errors could indicate a missing camera calibration file or a missing image.
+			collectors[label].incrErrors();
+			continue;
+		}
+		
+		if( !myProcessor.loadFeatureData(templatePath.c_str()) ) {
+			cout << "\E[31m" <<  "Could not set load feature data. Arg: " << "\e[0m" << templatePath << endl;
+			continue;
+		}
+		
+		if( !myProcessor.setTemplate(templatePath.c_str()) ) {
+			cout << "\E[31m" <<  "Could not set template. Arg: " << "\e[0m" << templatePath << endl;
+			continue;
+		}
+		
+		cout << "Outputting aligned image to: " << imgOutputPath << endl;
+		
+		if( !myProcessor.alignForm(imgOutputPath.c_str()) ) {
+			cout << "\E[31m" <<  "Could not align. Arg: " << "\e[0m" << imgOutputPath  << endl;
+			collectors[label].incrErrors();
+			continue;
+		}
+		
+		if( !myProcessor.processForm(jsonOutfile.c_str()) ) {
+			cout << "\E[31m" << "Could not process. Arg: " << "\e[0m" << jsonOutfile << endl;
+			collectors[label].incrErrors();
+			continue;
+		}
+		if( !MarkupForm::markupForm(jsonOutfile.c_str(), imgOutputPath.c_str(), markedupFormOutfile.c_str()) ) {
+			cout << "\E[31m" <<  "Could not markup. Arg: " << "\e[0m" << markedupFormOutfile  << endl;
+			collectors[label].incrErrors();
+			continue;
+		}
+		
+		final=clock()-init;
+		cout << "Time taken: " << (double)final / ((double)CLOCKS_PER_SEC) << " seconds" << endl;
+		cout << "\E[32m" << "Apparent success!" << "\e[0m" << endl;
+		
+		collectors[label].addTime( (double)final / ((double)CLOCKS_PER_SEC) );
+
+		if(fileExists(jsonOutfile) && fileExists(expectedJsonFile) && fileExists(templatePath + ".json")){
+			collectors[label].compareFiles(jsonOutfile, expectedJsonFile, COMP_BUBBLE_VALS);
+			collectors[label].compareFiles(jsonOutfile, templatePath + ".json", COMP_BUBBLE_OFFSETS);
+		}
+		else{
+			cout << "\E[31m" <<  "Could not compare files. File does not exist." << "\e[0m" << endl;
+			continue;
+		}
+	}
+	
+	//Print out overall stats:
+	#ifdef OUT_CSV_PATH
+		ofstream outfile(("aligned_forms/" + imageDir + OUT_CSV_PATH).c_str(), ios::out | ios::binary);
+	#endif	
+
+	cout << linebreak << endl;
+	StatCollector overall;
+	map<string, StatCollector>::iterator mapit;
+	for ( mapit=collectors.begin() ; mapit != collectors.end(); mapit++ ){
+		cout << (*mapit).first << endl << (*mapit).second;
+		overall += (*mapit).second;
+		#ifdef OUT_CSV_PATH
+			outfile << (*mapit).first << ", ";
+			(*mapit).second.printAsRow(outfile);
+		#endif
+	}
+	cout << "overall" << endl << overall;
+
+	#ifdef OUT_CSV_PATH
+		outfile.close();
+	#endif
+}
